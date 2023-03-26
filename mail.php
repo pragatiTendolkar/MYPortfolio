51b1c144eafc6280fc719f1a5814180fb6c97845
Pragati Tendolkar <95703908+pragatiTendolkar@users.noreply.github.com>
2023-03-26 08:36:47 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "pragati.t@digichefs.com";
+$to = "tendolkarpragati123@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
